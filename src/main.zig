52d9131a0186755fa9b67477f71cf1316c0ee34f
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-10-15 08:49:53 +0200

    Read from file

--- src/main.zig
+++ src/main.zig
@@ -9,7 +9,18 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = &gpa.allocator;
 
-    var parser = try Parser.init(" print(true)\t", std.testing.allocator);
+    var args = try std.process.argsAlloc(allocator);
+    defer allocator.free(args);
+
+    const file_name = if (args.len == 2) args[1] else {
+        try std.io.getStdOut().outStream().print("{} <source file>\n", .{args[0]});
+        return;
+    };
+
+    const source = try std.fs.cwd().readFileAlloc(allocator, file_name, 1_000_000);
+    defer allocator.free(source);
+
+    var parser = try Parser.init(source, std.testing.allocator);
     defer parser.deinit();
 
     const nodes = try parser.parse();
